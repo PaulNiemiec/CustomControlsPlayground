1ba41c4b037a5ffa462ab5806ca605a96465cde0
Paweł Niemiec <102246951+PaulNiemiec@users.noreply.github.com>
2023-09-07 11:21:24 +0200

    Project initialisation

---
+++ CustomControlsPlayground/CustomControlsPlayground/CustomControlsPlayground/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CustomControlsPlayground;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
